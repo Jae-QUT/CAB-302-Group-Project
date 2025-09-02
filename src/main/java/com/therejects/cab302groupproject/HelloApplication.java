356ffc13f7b2dd0ed6aee08a1cddf43c777576ef
Sean <n11076143@qut.edu.au>
2025-09-03 09:42:52 +1000

    Initial Login page using JavaFX

--- src/main/java/com/therejects/cab302groupproject/HelloApplication.java
+++ src/main/java/com/therejects/cab302groupproject/HelloApplication.java
@@ -5,19 +5,18 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-import java.io.IOException;
-
 public class HelloApplication extends Application {
     @Override
-    public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+    public void start(Stage stage) throws Exception {
+        FXMLLoader fxml = new FXMLLoader(HelloApplication.class.getResource("/ui/login-view.fxml"));
+        Scene scene = new Scene(fxml.load(), 980, 560);
+        stage.setTitle("<Math Monsters> — Sign in");
         stage.setScene(scene);
+        stage.centerOnScreen();
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }

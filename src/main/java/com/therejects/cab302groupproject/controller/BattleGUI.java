c72c3a6061acf763cee6d56ff162c603849c6a3c
Ryan <ryansu0823@gmail.com>
2025-09-14 02:13:57 +1000

    Implementing Question GUI to Battle GUI

--- src/main/java/com/therejects/cab302groupproject/controller/BattleGUI.java
+++ src/main/java/com/therejects/cab302groupproject/controller/BattleGUI.java
@@ -1,8 +1,12 @@
 package com.therejects.cab302groupproject.controller;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
 
 
 public class BattleGUI {
@@ -21,6 +25,20 @@ public class BattleGUI {
         fightBtn.setOnAction(e -> battleLog.setText("Choose a move..."));
         bagBtn.setOnAction(e -> battleLog.setText("Opening your bag..."));
         pokemonBtn.setOnAction(e -> battleLog.setText("Choose a Pokémon..."));
-        forfeitBtn.setOnAction(e -> battleLog.setText("You forfeited the battle!"));
+        forfeitBtn.setOnAction(e -> onGoToQuestionGen());
+    }
+
+    @FXML
+    private void onGoToQuestionGen() {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource(
+                    "/com/therejects/cab302groupproject/QuestionGen-view.fxml"
+            ));
+            Stage stage = (Stage) forfeitBtn.getScene().getWindow(); // any node from current scene
+            stage.setScene(new Scene(root)); // replace entire scene
+            stage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }

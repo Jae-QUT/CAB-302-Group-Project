15a2ae802d65713f43a72209417cc6f41f62e525
Ryan <ryansu0823@gmail.com>
2025-10-05 01:55:17 +1000

    - Finished Switching Mons - Implemented a monster selection screen at the start of battle - Added 5 Mons to Database - Moved some files around - Changed UserDao to use AuthDatabase instead of Database which was connected to mathmonsters.db
    # Conflicts:
    #	src/main/java/com/example/mon/app/MonDatabase.java

--- src/main/java/com/therejects/cab302groupproject/controller/BattleGUI.java
+++ src/main/java/com/therejects/cab302groupproject/controller/BattleGUI.java
@@ -224,6 +224,7 @@ public class BattleGUI extends QuestionGenerator {
         subMenu.setVisible(true);
     }
 
+    // create the universal "Back" button
     private Button createBackButton() {
         Button back = new Button("Back");
         back.setPrefWidth(200);
@@ -295,7 +296,7 @@ public class BattleGUI extends QuestionGenerator {
         popup.showAndWait();
 
         if (qGen.checkAnswer(ctrl.userAnswer)) {
-            enemyCurrentHp = Math.max(0, enemyCurrentHp - 10);
+            enemyCurrentHp = Math.max(0, enemyCurrentHp - 50);
             updateHpBars();
             finishAction("Correct! Attack landed.");
             if (!isBattleOver) {

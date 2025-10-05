e33c8473a1b5eb349adfd9071e627a3560dbac2c
Jae <n11620099@qut.edu.au>
2025-10-05 21:24:09 +1000

    First leaderboard upload
    # Conflicts:
    #	auth.db
    
    # Conflicts:
    #	src/main/java/com/therejects/cab302groupproject/controller/LeaderboardController.java
    #	src/main/resources/com/therejects/cab302groupproject/Leaderboard-view.fxml

--- src/main/java/com/therejects/cab302groupproject/controller/LeaderboardController.java
+++ src/main/java/com/therejects/cab302groupproject/controller/LeaderboardController.java
@@ -1,9 +1,7 @@
 package com.therejects.cab302groupproject.controller;
 
-import com.therejects.cab302groupproject.Navigation.*;
 import com.therejects.cab302groupproject.Leaderboard;
 import com.therejects.cab302groupproject.MainMenuLauncher;
-
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -13,7 +11,6 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-
 public class LeaderboardController {
     @FXML
     private Pagination leaderboardPage;
@@ -21,36 +18,17 @@ public class LeaderboardController {
     private Button backToMenu;
     @FXML
     private TextField usernameSearch;
-    @FXML
-    private GridPane leaderboardGrid;
-
-    private ScreenManager screenManager;
-
-    /**
-     *
-     * @return the screenManager if there is an issue injecting into the manager. It will return
-     */
-    public ScreenManager sm() {
-        if (screenManager == null) {
-            // fallback if someone forgot to inject; build from current window
-            Stage stage = (Stage) backToMenu.getScene().getWindow();
-            screenManager = new ScreenManager(stage);
-        }
-        return screenManager;
-    }
 
     @FXML
     protected void onBackToMainMenu() throws IOException {
-        sm().navigateTo("MAIN_MENU");
+        Stage stage = (Stage) backToMenu.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(Leaderboard.class.getResource("MainMenu.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), Leaderboard.WIDTH, Leaderboard.HEIGHT);
+        stage.setScene(scene);
     }
 
     @FXML
     protected void onUsernameSearch() {
-
-    }
-
-    @FXML
-    protected void leaderboardGrid() {
 
     }
 }
